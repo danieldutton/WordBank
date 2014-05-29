d59cc5d8baf5cfd1d34a24c00661c0f74f150a32
danieldutton <danielbdutton@hotmail.co.uk>
2014-05-29 11:23:36 +0100

    Integration tests complete for XmlWordBank.cs

--- WordBank.IntegrationTests/Mother.cs
+++ WordBank.IntegrationTests/Mother.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
 using System.Resources;
-using WordBank.Repository;
 
 namespace WordBank.IntegrationTests
 {
@@ -17,47 +16,25 @@ namespace WordBank.IntegrationTests
             return resMan;
         }
 
-        public static List<string> ExpectedWordSequence()
+        public static List<string> ExpectedWordKeySequence()
         {
             var WordsList = new List<string>
             {
-                "Word1", "Word2", "Word3", "Word4", "Word5",
-                "Word6", "Word7", "Word8", "Word9", "Word10",
+                "word1", "word2", "word3", "word4", "word5",
+                "word6", "word7", "word8", "word9", "word10",
             };
 
             return WordsList;
         }
 
-        public static Queue<WordAnswer> ExpectedWordQueue()
+        public static List<int> ExpectedIdSequence()
         {
-            var worddQueue = new Queue<WordAnswer>();
-
-            worddQueue.Enqueue(new WordAnswer(1, "word1", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(2, "word2", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(3, "word3", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(4, "word4", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(5, "word5", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(6, "word6", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(7, "word7", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(8, "word8", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(9, "word9", string.Empty));
-            worddQueue.Enqueue(new WordAnswer(10, "word10", string.Empty));
-
-            return worddQueue;
-        }  
-
-        public static Dictionary<string, string> ExpectedWordMap()
-        {
-            var dic = new Dictionary<string, string>
+            var idList = new List<int>
             {
-                {"Word1", string.Empty}, {"Word2", string.Empty},
-                {"Word3", string.Empty}, {"Word4", string.Empty},
-                {"Word5", string.Empty}, {"Word6", string.Empty},
-                {"Word7", string.Empty}, {"Word8", string.Empty},
-                {"Word9", string.Empty}, {"Word10", string.Empty},
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
             };
 
-            return dic;
-        } 
+            return idList;
+        }
     }
 }
--- WordBank.IntegrationTests/Repository/XmlWordBank_Should.cs
+++ WordBank.IntegrationTests/Repository/XmlWordBank_Should.cs
@@ -27,67 +27,211 @@ namespace WordBank.IntegrationTests.Repository
             _sut = new XmlWordBank(_xDocumentParser);
         }
 
+
         [Test]
-        public void InitialiseWordBank_GivePropertyWordMap_10KeyValues()
+        public void InitialiseWordBank_InitPropertyWordMap_With10Items()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            Dictionary<string, string> wMap = _sut.WordMap;
+
+            Assert.AreEqual(10, wMap.Count);
+        }
+
+        [Test]
+        public void InitialiseWordBank_InitPropertyWordMap_WithKeySequence()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            List<string> keyList = _sut.WordMap.Select(x => x.Key).ToList();
+
+            Assert.AreEqual(Mother.ExpectedWordKeySequence(), keyList);
+        }
+
+        [Test]
+        public void InitialiseWordBank_InitPropertyWordMap_WithCorrectValueSequence()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            List<string> valueList = _sut.WordMap.Select(x => x.Value).ToList();
+
+            Assert.IsTrue(valueList.TrueForAll(y => y.Equals(string.Empty)));
+        }
+
+        [Test]
+        public void InitialiseWordBank_InitPropertyWordQueue_With10Items()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            Queue<WordAnswer> wordQueue = _sut.WordQueue;
+
+            Assert.AreEqual(10, wordQueue.Count);
+            CollectionAssert.AllItemsAreInstancesOfType(wordQueue, typeof(WordAnswer));
+        }
+
+        [Test]
+        public void InitialiseWordBank_InitPropertyWordQueue_WithCorrectIdSequence()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            List<int> idList = _sut.WordQueue.Select(x => x.Id).ToList();
+
+            Assert.AreEqual(Mother.ExpectedIdSequence(), idList);
+        }
+
+        [Test]
+        public void InitialiseWordBank_InitPropertyWordQueue_WithCorrectTextSequence()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            List<string> wordList = _sut.WordQueue.Select(x => x.Word).ToList();
+
+            Assert.AreEqual(Mother.ExpectedWordKeySequence(), wordList);
+        }
+
+        [Test]
+        public void InitialiseWordBank_InitPropertyWordQueue_WithCorrectAnswerSequence()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            List<string> answerList = _sut.WordQueue.Select(x => x.Answer).ToList();
+
+            Assert.IsTrue(answerList.All(x => x == string.Empty));
+        }
+
+        //sample of 5
+        [Test]
+        public void GetWord_ReturnOneWordAnswer_WithCorrectIdFor_FirstWord()
+        {
+            _sut.InitialiseWordBank(_xmlResource);
+            WordAnswer result = _sut.GetWord();
+
+            Assert.AreEqual(1, result.Id);
+        }
+
+        [Test]
+        public void GetWord_ReturnOneWordAnswer_WithCorrectIdFor_SecondWord()
         {
             _sut.InitialiseWordBank(_xmlResource);
 
-            Dictionary<string, string> result = _sut.WordMap;
+            _sut.GetWord();
+            WordAnswer result = _sut.GetWord();
 
-            Assert.AreEqual(10, result.Count);
+            Assert.AreEqual(2, result.Id);
         }
 
         [Test]
-        public void InitialiseWordBank_GivePropertyWordMapTheCorrectKeyValues()
+        public void GetWord_ReturnOneWordAnswer_WithCorrectIdFor_ThirdWord()
         {
             _sut.InitialiseWordBank(_xmlResource);
+            _sut.GetWord();
+            _sut.GetWord();
+            WordAnswer result = _sut.GetWord();
 
-            Dictionary<string, string> result = _sut.WordMap;
-
-            CollectionAssert.AreEqual(Mother.ExpectedWordMap(), result);
+            Assert.AreEqual(3, result.Id);
         }
 
         [Test]
-        public void InitialiseWordBank_GivePropertyWordQueue_10WordAnswerItems()
+        public void GetWord_ReturnOneWordAnswer_WithCorrectIdFor_FourthWord()
         {
             _sut.InitialiseWordBank(_xmlResource);
+            _sut.GetWord();
+            _sut.GetWord();
+            _sut.GetWord();
+            WordAnswer result = _sut.GetWord();
 
-            Queue<WordAnswer> result = _sut.WordQueue;
-
-            Assert.AreEqual(10, result.Count);
+            Assert.AreEqual(4, result.Id);
         }
 
         [Test]
-        public void InitialiseWordBank_WithCorrectSequenceOfWords_Word1()
+        public void GetWord_ReturnOneWordAnswer_WithCorrectIdFor_FifthWord()
         {
             _sut.InitialiseWordBank(_xmlResource);
+            _sut.GetWord();
+            _sut.GetWord();
+            _sut.GetWord();
+            _sut.GetWord();
+            WordAnswer result = _sut.GetWord();
 
-            Queue<WordAnswer> result = _sut.WordQueue;
-
-            CollectionAssert.AreEqual(Mother.ExpectedWordQueue(), result);
+            Assert.AreEqual(5, result.Id);
         }
 
         [Test]
-        public void InitialiseWordBank_WordAnswerContainTheCorrectId()
+        public void GetWord_FireIsEmptyEvent_IfQueueIsEmpty()
+        {
+            bool wasFired = false;
+            var emptyQueue = new Queue<WordAnswer>();
+            _sut.IsEmpty += (o, e) => wasFired = true;
+
+            _sut.WordQueue = emptyQueue;
+            _sut.GetWord();
+
+            Assert.IsTrue(wasFired);
+        }
+
+        [Test]
+        public void SubmitAnswer_FailtToUpdateWordMap_IfWordAnswerIsNull()
         {
             _sut.InitialiseWordBank(_xmlResource);
+            _sut.SubmitAnswer(null);
 
-            var result = _sut.WordQueue.ToList();
+            Assert.AreEqual(10, _sut.WordMap.Count);
+        }
+
+        //sample of 5
+        [Test]
+        public void SubmitAnswer_UpdateWordMapAnswer_ForWordAnswer1()
+        {
+            var wordAnswer = new WordAnswer(1, "word1", "answer1");
+
+            _sut.InitialiseWordBank(_xmlResource);
+            _sut.SubmitAnswer(wordAnswer);
+            Dictionary<string, string> wMap = _sut.WordMap;
+
+            Assert.AreEqual("answer1", wMap["word1"]);
         }
 
         [Test]
-        public void InitialiseWordBank_WordAnswerContainTheCorrectText()
+        public void SubmitAnswer_UpdateWordMapAnswer_ForWordAnswer2()
         {
-            
+            var wordAnswer = new WordAnswer(1, "word2", "answer2");
+
+            _sut.InitialiseWordBank(_xmlResource);
+            _sut.SubmitAnswer(wordAnswer);
+            Dictionary<string, string> wMap = _sut.WordMap;
+
+            Assert.AreEqual("answer2", wMap["word2"]);
         }
 
         [Test]
-        public void InitialiseWordBank_WordAnswersAnswerStringEmptyByDefault()
+        public void SubmitAnswer_UpdateWordMapAnswer_ForWordAnswer3()
         {
-            
+            var wordAnswer = new WordAnswer(1, "word3", "answer3");
+
+            _sut.InitialiseWordBank(_xmlResource);
+            _sut.SubmitAnswer(wordAnswer);
+            Dictionary<string, string> wMap = _sut.WordMap;
+
+            Assert.AreEqual("answer3", wMap["word3"]);
         }
-        
-        //consider not tearing down xmlresource load to save time
+
+        [Test]
+        public void SubmitAnswer_UpdateWordMapAnswer_ForWordAnswer4()
+        {
+            var wordAnswer = new WordAnswer(1, "word4", "answer4");
+
+            _sut.InitialiseWordBank(_xmlResource);
+            _sut.SubmitAnswer(wordAnswer);
+            Dictionary<string, string> wMap = _sut.WordMap;
+
+            Assert.AreEqual("answer4", wMap["word4"]);
+        }
+
+        [Test]
+        public void SubmitAnswer_UpdateWordMapAnswer_ForWordAnswer5()
+        {
+            var wordAnswer = new WordAnswer(1, "word5", "answer5");
+
+            _sut.InitialiseWordBank(_xmlResource);
+            _sut.SubmitAnswer(wordAnswer);
+            Dictionary<string, string> wMap = _sut.WordMap;
+
+            Assert.AreEqual("answer5", wMap["word5"]);
+        }
+
         [TearDown]
         public void TearDown()
         {
